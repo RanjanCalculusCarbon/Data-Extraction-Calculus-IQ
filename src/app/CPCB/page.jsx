0b7548a005e3c38cefa45d0bf2bde6e907424cc1
RanjanCalculusCarbon <ranjan@calculuscarbon.com>
2023-10-05 12:19:59 +0530

    Initiated

---
+++ src/app/CPCB/page.jsx
@@ -0,0 +1,149 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+// Convert JSON data to CSV format
+function convertToCSV(json) {
+    const header = json.headerContent.map((h) => `"${h.label}"`).join(",");
+    const body = json.bodyContent
+        .map((row) => {
+            return json.headerContent
+                .map((header) => `"${row[header.key] || ""}"`)
+                .join(",");
+        })
+        .join("\n");
+
+    return `${header}\n${body}`;
+}
+
+// Trigger a download for CSV content
+function triggerCSVDownload(data, filename = "data.csv") {
+    const csv = convertToCSV(data);
+    const blob = new Blob([csv], { type: "text/csv" });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.setAttribute("hidden", "");
+    a.setAttribute("href", url);
+    a.setAttribute("download", filename);
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+// Skeleton Loader Component for data loading placeholder
+function SkeletonLoader() {
+    return (
+        <>
+            <div className="animate-pulse flex space-x-4">
+                <div className="flex-1 space-y-4 py-1">
+                    <div className="h-4 bg-gray-400 rounded w-3/4"></div>
+                    <div className="space-y-2">
+                        {[...Array(5)].map((_, i) => (
+                            <div key={i} className="h-4 bg-gray-400 rounded w-full"></div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+// DataTable Component to render data in table format
+function DataTable({ data }) {
+    if (!data) return <SkeletonLoader />;
+
+    return (
+        <table className="min-w-full bg-white border border-gray-300">
+            <thead>
+                <tr>
+                    {data.headerContent.map((header) => (
+                        <th key={header.key} className="border border-gray-300 px-4 py-2">
+                            {header.label}
+                        </th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {data.bodyContent &&
+                    data.bodyContent.length > 0 &&
+                    data.bodyContent.map((row, index) => (
+                        <tr key={index}>
+                            {data.headerContent.map((header) => (
+                                <td
+                                    key={header.key}
+                                    className="border border-gray-300 px-4 py-2"
+                                >
+                                    {row[header.key] || "-"}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+            </tbody>
+        </table>
+    );
+};
+
+// Main Home Component
+export default function Home() {
+    const [firstAPI, setFirstAPI] = useState(null);
+    const [secondAPI, setSecondAPI] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const urls = [
+                    "https://eprplastic.cpcb.gov.in/epr/api/v1.0/pibo/list_national_cpcb_dashboard_applications",
+                    "https://eprplastic.cpcb.gov.in/epr/api/v1.0/pibo/list_national_cpcb_epr_target",
+                ];
+                const postData = { year: "2022-23" };
+                const [response1, response2] = await Promise.all(
+                    urls.map((url) => axios.post(url, postData))
+                );
+
+                setFirstAPI(response1.data.piboRegistrationList);
+                setSecondAPI(response2.data.piboRegistrationList);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            };
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            <div className='mt-6 text-6xl text-center bold'>CPCB DATA</div>
+        <div className="p-8">
+            <h2 className="text-xl font-bold mb-4">
+                Application Status for PIBO Registration (F.Y. 22-23)
+            </h2>
+            {firstAPI ? (
+                <button
+                onClick={() => triggerCSVDownload(firstAPI, "firstAPI_data.csv")}
+                className="bg-blue-500 text-white px-4 py-2 mb-4"
+                >
+                    Download CSV
+                </button>
+            ) : (
+                <div className="bg-gray-400 animate-pulse w-32 h-8 rounded mb-4"></div>
+                )}
+            <DataTable data={firstAPI} />
+            <h2 className="text-xl font-bold mt-8 mb-4">
+                EPR Target of Registered PIBOs (F.Y. 22-23)
+            </h2>
+            {secondAPI ? (
+                <button
+                onClick={() => triggerCSVDownload(secondAPI, "secondAPI_data.csv")}
+                className="bg-blue-500 text-white px-4 py-2 mb-4"
+                >
+                    Download CSV
+                </button>
+            ) : (
+                <div className="bg-gray-400 animate-pulse w-32 h-8 rounded mb-4"></div>
+                )}
+            <DataTable data={secondAPI} />
+        </div>
+                </>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,146 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+// Convert JSON data to CSV format
+function convertToCSV(json) {
+  const header = json.headerContent.map((h) => `"${h.label}"`).join(",");
+  const body = json.bodyContent
+    .map((row) => {
+      return json.headerContent
+        .map((header) => `"${row[header.key] || ""}"`)
+        .join(",");
+    })
+    .join("\n");
+
+  return `${header}\n${body}`;
+}
+
+// Trigger a download for CSV content
+function triggerCSVDownload(data, filename = "data.csv") {
+  const csv = convertToCSV(data);
+  const blob = new Blob([csv], { type: "text/csv" });
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.setAttribute("hidden", "");
+  a.setAttribute("href", url);
+  a.setAttribute("download", filename);
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
+// Skeleton Loader Component for data loading placeholder
+function SkeletonLoader() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="animate-pulse flex space-x-4">
+        <div className="flex-1 space-y-4 py-1">
+          <div className="h-4 bg-gray-400 rounded w-3/4"></div>
+          <div className="space-y-2">
+            {[...Array(5)].map((_, i) => (
+              <div key={i} className="h-4 bg-gray-400 rounded w-full"></div>
+            ))}
+          </div>
         </div>
       </div>
+    </>
+  );
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+// DataTable Component to render data in table format
+function DataTable({ data }) {
+  if (!data) return <SkeletonLoader />;
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+  return (
+    <table className="min-w-full bg-white border border-gray-300">
+      <thead>
+        <tr>
+          {data.headerContent.map((header) => (
+            <th key={header.key} className="border border-gray-300 px-4 py-2">
+              {header.label}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.bodyContent &&
+          data.bodyContent.length > 0 &&
+          data.bodyContent.map((row, index) => (
+            <tr key={index}>
+              {data.headerContent.map((header) => (
+                <td
+                  key={header.key}
+                  className="border border-gray-300 px-4 py-2"
+                >
+                  {row[header.key] || "-"}
+                </td>
+              ))}
+            </tr>
+          ))}
+      </tbody>
+    </table>
+  );
+};
+
+// Main Home Component
+export default function Home() {
+  const [firstAPI, setFirstAPI] = useState(null);
+  const [secondAPI, setSecondAPI] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const urls = [
+          "https://eprplastic.cpcb.gov.in/epr/api/v1.0/pibo/list_national_cpcb_dashboard_applications",
+          "https://eprplastic.cpcb.gov.in/epr/api/v1.0/pibo/list_national_cpcb_epr_target",
+        ];
+        const postData = { year: "2022-23" };
+        const [response1, response2] = await Promise.all(
+          urls.map((url) => axios.post(url, postData))
+        );
+
+        setFirstAPI(response1.data.piboRegistrationList);
+        setSecondAPI(response2.data.piboRegistrationList);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      };
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="p-8">
+      <h2 className="text-xl font-bold mb-4">
+        Application Status for PIBO Registration (F.Y. 22-23)
+      </h2>
+      {firstAPI ? (
+        <button
+          onClick={() => triggerCSVDownload(firstAPI, "firstAPI_data.csv")}
+          className="bg-blue-500 text-white px-4 py-2 mb-4"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
+          Download CSV
+        </button>
+      ) : (
+        <div className="bg-gray-400 animate-pulse w-32 h-8 rounded mb-4"></div>
+      )}
+      <DataTable data={firstAPI} />
+      <h2 className="text-xl font-bold mt-8 mb-4">
+        EPR Target of Registered PIBOs (F.Y. 22-23)
+      </h2>
+      {secondAPI ? (
+        <button
+          onClick={() => triggerCSVDownload(secondAPI, "secondAPI_data.csv")}
+          className="bg-blue-500 text-white px-4 py-2 mb-4"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          Download CSV
+        </button>
+      ) : (
+        <div className="bg-gray-400 animate-pulse w-32 h-8 rounded mb-4"></div>
+      )}
+      <DataTable data={secondAPI} />
+    </div>
+  );
 }
